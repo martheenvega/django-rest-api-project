dd0355e4f7b03f0f0ce6489a6b769cac227109c6
Martín Hernández <mhv.soporte@gmail.com>
2024-02-26 17:35:21 -0600

    Primeras modificaciones

--- source/ApiProject/blog/models.py
+++ source/ApiProject/blog/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """docstring for UserManager."""
+
+    def create_user(self, email, password=None, extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, extra_fields)
+
+    def __init__(self, arg):
+        super(UserManager, self).__init__()
+        self.arg = arg
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """docstring for User."""
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def __init__(self, arg):
+        super(User, self).__init__()
+        self.arg = arg
+
+class CustomUserManager(BaseUserManager):
+    """docstring for CustomUserManager."""
+    pass
+
+    def create_user(self, email, password=None, extra_fields):
+        """
+        Create and return a User with the given email and password
+        """
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, extra_fields):
+        """
+        Create and return a superuser with the gicen email and password
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, extra_fields)
+
+    def __init__(self, arg):
+        super(CustomUserManager, self).__init__()
+        self.arg = arg
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """docstring for CustomUser."""
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = CustomUserManager()
+
+    def __init__(self, arg):
+        super(CustomUser, self).__init__()
+        self.arg = arg
